24de3d05ec17f8a6e1ef6a48a381744b66fd8899
62523 <625237127@qq.com>
2023-12-21 16:45:22 +0800

    workflow

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from src.app import app
+
+class AppTestCase(unittest.TestCase):
+    def test_example(self):
+        print('hello-world')
+        print('Athenavi')
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
